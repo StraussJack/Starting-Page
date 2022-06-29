0bc29f7214045553c99760e408b65fccc5a3739e
Sergey Ermolin <61907391+StraussJack@users.noreply.github.com>
2022-06-29 16:20:02 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 let box1 = document.getElementById("box1");
 let box2 = document.getElementById("box2");
+let name = document.getElementById("name");
+let back = document.getElementById("back");
 
 box1.addEventListener("mouseenter", function () {
-  this.style.color = "red";
+  this.style.color = "#aa80ff";
 });
 box1.addEventListener("mouseleave", function () {
   this.style.color = "black";
 });
 
 box2.addEventListener("mouseenter", function () {
-  this.style.color = "red";
+  this.style.color = "#aa80ff";
 });
 box2.addEventListener("mouseleave", function () {
+  this.style.color = "black";
+});
+name.addEventListener("mouseenter", function () {
+  this.style.color = "#aa80ff";
+});
+name.addEventListener("mouseleave", function () {
+  this.style.color = "black";
+});
+back.addEventListener("mouseenter", function () {
+  this.style.color = "#aa80ff";
+});
+back.addEventListener("mouseleave", function () {
   this.style.color = "black";
 });
